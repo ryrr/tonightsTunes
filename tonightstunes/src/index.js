32e77789185ff3eea8636264d3d3144d906dabf6
RiPRR <rpr288@nyu.edu>
2020-04-03 01:34:14 -0400

    inti

---
+++ tonightstunes/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+let Track = (props) => {
+    return (
+        <div className='track'>
+            b
+        </div>
+    )
+}
+let App = (props) => {
+    return (
+        <div className='body'>
+
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <Track />,
+    document.getElementById('root')
+);
