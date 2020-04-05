defcc1fb5509f3ff3be51d1b6fb9276dc9741a09
RiPRR <rpr288@nyu.edu>
2020-04-05 00:46:18 -0400

    basics started

--- tonightstunes/src/index.js
+++ tonightstunes/src/index.js
@@ -3,22 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-let Track = (props) => {
+let App = (props) => {
     return (
-        <div className='track'>
-            b
+        <div className='app'>
+            <Heading></Heading>
+            <Track></Track>
         </div>
     )
 }
-let App = (props) => {
-    return (
-        <div className='body'>
 
+let Heading = (props) => {
+    return (
+        <div className='heading'>
+            <h1 className='title'>Whip</h1>
+            <h3 className='subTitle'>A playlist generator based on music near you</h3>
+        </div>
+
+    )
+}
+let Track = (props) => {
+    return (
+        <div className='track'>
+            <div className='albumArt'>
+            </div>
+            <div className='trackInfo'>
+                <h4 className='trackName'>Butterfly Effect</h4>
+                <div className='trackName2'>
+                    <h5 className=''>Travis Scott</h5>
+                    <h5 className=''>3:45</h5>
+                </div>
+            </div>
         </div>
     )
 }
 
 ReactDOM.render(
-    <Track />,
+    <App />,
     document.getElementById('root')
 );
