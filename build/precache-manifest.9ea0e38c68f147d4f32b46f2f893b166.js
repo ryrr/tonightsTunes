67a0bf02b417a725184bfabf8ecee0b11e653131
RiPRR <rpr288@nyu.edu>
2020-04-30 02:17:52 -0400

    build attempt

---
+++ build/precache-manifest.9ea0e38c68f147d4f32b46f2f893b166.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e65c49e6cd15139c86d8935ed12360a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9b1aedd64cd595751ce",
+    "url": "/static/css/main.6bc3e714.chunk.css"
+  },
+  {
+    "revision": "9a9049734a1521ea866b",
+    "url": "/static/js/2.217ec2a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.217ec2a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b1aedd64cd595751ce",
+    "url": "/static/js/main.cf33f1fe.chunk.js"
+  },
+  {
+    "revision": "bf1e1d4abe594d91cd88",
+    "url": "/static/js/runtime-main.54db4421.js"
+  },
+  {
+    "revision": "64b77d78856ef510c7e01c0b21937808",
+    "url": "/static/media/Curvilingus.64b77d78.otf"
+  }
+]);
